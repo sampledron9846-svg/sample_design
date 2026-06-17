d8d941553183fb93b554ae1f3300a7d3e4af7e15
sampledron9846-svg <sampledron9846@gmail.com>
2026-06-17 20:53:38 +0200

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,27 +1,40 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2026 Dron Sankhala
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_4bit_counter (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    output wire [7:0] uio_oe,   // IOs: Enable path
+    input  wire       ena,      // Enable
+    input  wire       clk,      // Clock
+    input  wire       rst_n     // Active-low reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    reg [3:0] counter;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    always @(posedge clk) begin
+        if (!rst_n)
+            counter <= 4'b0000;
+        else if (ena)
+            counter <= counter + 1'b1;
+    end
+
+    // Display counter on lower four output pins
+    assign uo_out = {4'b0000, counter};
+
+    // Unused bidirectional pins
+    assign uio_out = 8'b00000000;
+    assign uio_oe  = 8'b00000000;
+
+    // Prevent unused input warnings
+    wire _unused = &{ui_in, uio_in, 1'b0};
 
 endmodule
+
+`default_nettype wire
